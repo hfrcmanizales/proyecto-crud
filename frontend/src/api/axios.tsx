4f676f780e7f85b8d3a5d0df5541f49c65894014
fernando <fernandorestrepo@hotmail.es>
2025-05-16 10:42:57 -0500

    agrege url

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,5 +1,5 @@
 import axios from "./instancia";
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_BACKEND;
 
 
 export const enviaData = (data) => axios.post(`${API_URL}/register`, data);
