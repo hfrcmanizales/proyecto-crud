323660b1317f9f9d371e14e5b29422f59206d656
fernando <fernandorestrepo@hotmail.es>
2025-05-20 20:52:42 -0500

    rutas

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    // Solo afecta desarrollo local, pero lo dejamos por buenas prácticas
+    historyApiFallback: true,
+  }
 })
